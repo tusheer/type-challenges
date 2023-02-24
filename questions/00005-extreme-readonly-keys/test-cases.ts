c0210b48054998e72f5df38ec46e4df4567a6dbc
tusheer <jaanealamtusher@gmail.com>
2023-02-24 18:27:58 +0600

    solve 00006-hard-simple-vue

--- questions/00005-extreme-readonly-keys/test-cases.ts
+++ questions/00005-extreme-readonly-keys/test-cases.ts
@@ -22,3 +22,4 @@ type EqualType<T, P extends keyof T> = Equal<Pick<T, P>, { readonly [I in P ]: T
 type GetReadonlyKeys<T> = keyof {
   [I in keyof T as EqualType<T, I> extends true ? I : never]: T[I]
 }
+
--- questions/00006-hard-simple-vue/template.ts
+++ questions/00006-hard-simple-vue/template.ts
@@ -1,1 +1,10 @@
-declare function SimpleVue(options: any): any
+declare function SimpleVue< Data, Computed, Methods>(options: {
+  data?: (this: void) => Data
+  computed?: Computed & ThisType<Methods & Data>
+  methods?: Methods & ThisType<Data & ComputedType<Computed> & Methods>
+}): any
+
+type ComputedType<C> = {
+  [ I in keyof C]: C[I] extends (t: any) => infer B ? B : never
+}
+
