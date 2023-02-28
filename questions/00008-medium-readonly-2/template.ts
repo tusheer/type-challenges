dd4584e72802235d973f1c88cc8b35317ac6b031
tusheer <jaanealamtusher@gmail.com>
2023-02-28 15:52:42 +0600

    solve 00008-medium-readonly-2

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,6 @@
-type MyReadonly2<T, K> = any
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  [Key in keyof T as Key extends K ? never : Key]: T[Key];
+} & {
+  readonly [Key in keyof T as Key extends K ? Key : never]: T[Key];
+}
+
