5751c9f95143100eeaed398eeb7b639cae68f537
tusheer <jaanealamtusher@gmail.com>
2023-03-01 23:56:13 +0600

    solve 00012-medium-chainable-options

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,4 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<Ob = {}> = {
+  option<Value, Key extends string> (key: Key, value: Value): Chainable<Omit<Ob, Key> & Record<Key, Value>>
+  get(): Ob
 }
