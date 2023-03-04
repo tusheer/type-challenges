53163f7f08455e6b6db9c54101a699cda76fa239
tusheer <jaanealamtusher@gmail.com>
2023-03-04 16:21:43 +0600

    solved 00017-hard-currying-1

--- questions/00017-hard-currying-1/template.ts
+++ questions/00017-hard-currying-1/template.ts
@@ -1,1 +1,10 @@
-declare function Currying(fn: any): any
+type Lastt<L> = L extends [...infer Fists, infer Last] ? Last : never
+
+type POPP<L> = L extends [...infer Fists, infer Last] ? [...Fists] : never
+
+type Curry<Fn extends Function, Return = ReturnType<Fn>, Parameteter = Parameters<Fn>> = Fn extends () => any ? Fn : Parameteter extends []
+  ? Return
+  : Curry<Fn, (value: Lastt<Parameteter>) => Return, POPP<Parameteter> >
+
+declare function Currying<Fn extends Function>(fn: Fn): Curry<Fn>
+
